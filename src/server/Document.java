6ba4a4956682b6be26efd67cfd264225db039e70
Deepak <Deepak@18.189.60.215>
2012-11-21 16:46:26 -0500

    Added Document and Paragraph classes.

---
+++ src/server/Document.java
@@ -0,0 +1,56 @@
+package server;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Document {
+	
+	private String documentID;
+	private Map<String, Paragraph> paragraphs;
+
+	/**
+	 * Constructor of the class Document
+	 * @param documentID String representing the document ID of the document
+	 */
+	public Document(String documentID) {
+		this.documentID = documentID;
+		this.paragraphs = new HashMap<String, Paragraph> ();
+	}
+	
+	/**
+	 * Returns the document ID of the particular document
+	 * @return String representing the document ID of the document
+	 */
+	public String getDocumentID() {
+		return documentID;
+	}
+	
+	/**
+	 * Returns a paragraph with the given paragraph ID if it already exists.
+	 * Otherwise, creates a new paragraph with the given paragraph ID and returns it. Updates document to contain the new paragraph as well.
+	 * @param paragraphID String representing the paragraph ID of the Paragraph object in the document
+	 * @return A Paragraph object that either already exists in the document, or is newly created with input paragraph ID
+	 */
+	public Paragraph getParagraph(String paragraphID) {
+		if (paragraphs.containsKey(paragraphID)) {
+			return paragraphs.get(paragraphID);
+		} else {
+			Paragraph newParagraph = new Paragraph(paragraphID);
+			paragraphs.put(paragraphID, newParagraph);
+			return newParagraph;
+		}
+		
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder documentText = new StringBuilder();
+		Set<String> paragraphKeys = paragraphs.keySet();
+		for (String paragraphKey : paragraphKeys) {
+			Paragraph paragraph = paragraphs.get(paragraphKey);
+			documentText.append(paragraph.toString());
+		}
+		return documentText.toString();
+	}
+}
---
+++ src/server/Paragraph.java
@@ -0,0 +1,36 @@
+package server;
+
+public class Paragraph {
+	private final String paragraphID;
+	private String paragraphText;
+	
+	/**
+	 * Constructor for the class paragraph
+	 * @param paragraphID String representing the paragraph ID of the paragraph
+	 */
+	public Paragraph(String paragraphID) {
+		this.paragraphID = paragraphID;
+		this.paragraphText = "";
+	}
+	
+	/**
+	 * Returns the paragraph ID of the particular paragraph
+	 * @return String representing paragraphID of the paragraph
+	 */
+	public String getParagraphID() {
+		return paragraphID;
+	}
+	
+	/**
+	 * Returns the text in a paragraph
+	 * @return String representing the text contained in the paragraph
+	 */
+	public String getParagraphText() {
+		return paragraphText;
+	}
+	
+	@Override
+	public String toString() {
+		return paragraphText;
+	}
+}
