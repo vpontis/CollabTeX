a7550f6ddd39780d9b20e7fda26c5cf4768ce384
Deepak <Deepak@18.189.60.215>
2012-11-21 23:36:52 -0500

    Made changes to the server.

--- src/server/DocumentTest.java
+++ src/server/DocumentTest.java
@@ -1,7 +1,5 @@
 package server;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class DocumentTest {
--- src/server/EtherpadServer.java
+++ src/server/EtherpadServer.java
@@ -1,7 +1,11 @@
 package server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,7 +19,6 @@ public class EtherpadServer {
 	private Map<String, String> user_passwordMappings;
 	
 	public EtherpadServer() throws IOException {
-		this.port = port;
 		this.currentDocuments = new ArrayList<Document> ();
 		this.serverSocket = new ServerSocket(port);
 		this.user_passwordMappings = new HashMap<String,String> ();
@@ -28,6 +31,44 @@ public class EtherpadServer {
 		this.user_passwordMappings = new HashMap<String, String> ();
 	}
 	
+	/**
+     * Run the server, listening for client connections and handling them.  
+     * Never returns unless an exception is thrown.
+     * @throws IOException if the main server socket is broken
+     * (IOExceptions from individual clients do *not* terminate serve()).
+     */
+    public void serve() throws IOException {
+        while (true) {
+            // block until a client connects
+            Socket socket = serverSocket.accept();
+            Thread socketThread = new Thread(new RunnableMines(socket));
+            socketThread.start();
+        }
+    }
+    
+    /**
+     * Class defined to help start a new thread every time a new client connects to the server
+     * @author Deepak
+     *
+     */
+    private class RunnableMines implements Runnable {
+    	Socket socket;
+    	
+    	public RunnableMines(Socket socket) {
+    		this.socket = socket;
+    	}
+    	
+		@Override
+		public void run() {
+			try {
+				handleConnection(socket);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}			
+		}
+    	
+    }
+	
 	private String handleRequest(String input) {
 		if (input.startsWith("login")) {
 			String[] tokens = input.split(" ");
@@ -38,8 +79,9 @@ public class EtherpadServer {
 				if (password.equals(expectedPassword)) {
 					return "Login success";
 				}
+				return "Wrong password";
 			}
-			return "Login failed";
+			return "Username does not exist";
 		} else if (input.startsWith("logon")) {
 			String[] tokens = input.split(" ");
 			String userName = tokens[1];
@@ -50,7 +92,18 @@ public class EtherpadServer {
 		throw new UnsupportedOperationException();
 	}
 	
-	public void addDocument(Document newDocument, String userName) {
+	public void handleConnection(Socket socket) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        
+        for (String line = in.readLine(); line!=null; line=in.readLine()) {
+            String output = handleRequest(line);
+            out.print(output);
+            out.flush();
+        }
+	}
+	
+	public void addDocument(Document newDocument) {
 		currentDocuments.add(newDocument);
 	}
 	
