3f8918dd10e5e5d96664afd6287ab61b71f3e104
Victor Pontis <vpontis@gmail.com>
2012-11-24 19:22:00 -0500

    added model package

---
+++ src/model/DocumentTest.java
@@ -0,0 +1,37 @@
+package model;
+
+import org.junit.Test;
+
+public class DocumentTest {
+
+	@Test
+	public void testSingleParagraphDocument() {
+		Document testingDocument = new Document("123", "Test1"); //Initialize a new document with a fake document ID
+		Paragraph newParagraph = testingDocument.getParagraph("1");
+		newParagraph.setParagraphText("This is a testing document");
+		System.out.println(testingDocument);
+	}
+	
+	@Test
+	public void testMultipleParagraphDocument() {
+		Document testingDocument = new Document("123", "Test2"); //Initialize a new document with a fake document ID
+		
+		Paragraph newParagraph1 = testingDocument.getParagraph("1");
+		newParagraph1.setParagraphText("This is a line from Paragraph 1");
+		
+		Paragraph newParagraph2 = testingDocument.getParagraph("2");
+		newParagraph2.setParagraphText("This is a line from Paragraph 2");
+		
+		System.out.println(testingDocument);
+	}
+	
+	@Test
+	public void testDate() {
+		Document testingDocument = new Document("123", "Test3"); //Initialize a new document with a fake document ID
+		
+		testingDocument.setLastEditDateTime();
+		System.out.println(testingDocument.getLastEditDateTime());
+		
+	}
+
+}
