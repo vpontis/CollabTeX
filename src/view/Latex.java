8f7412d68453be5272593e0e5ae502b480077556
Victor Pontis <vpontis@gmail.com>
2012-12-09 22:35:06 -0500

    added line breaking support

--- src/view/Latex.java
+++ src/view/Latex.java
@@ -5,6 +5,11 @@ import java.awt.Insets;
 import org.scilab.forge.jlatexmath.*;
 
 public class Latex {
+	/**
+	 * Tests is a certain string is latex code
+	 * @param string
+	 * @return if the string is valid latex code
+	 */
 	public static boolean isLatex(String string){
 		try{
 			new TeXFormula(string);
@@ -15,12 +20,21 @@ public class Latex {
 		}
 	}
 	
+	/**
+	 * Converts a string of latex into a TeXIcon from which an image
+	 * can be generated
+	 * @param latexString from the document area
+	 * @return TeXIcon that can build an image from the given latex
+	 */
 	public static TeXIcon getLatex(String latexString){
+		//we only care about latex embedded in $$, all else is regular text
 		String[] stringSplit = latexString.split("\\$\\$");
+		
 		StringBuilder out = new StringBuilder();
-//		String beginning = "\\begin{tabular}{m{1cm}}";
+		
 		String beginning = "\\begin{array}{l} ";
 		String end = "\\end{array}";
+		
 		out.append(beginning);
 		for (int i = 0; i < stringSplit.length; i++ ){
 			if (i % 2 == 0){
@@ -32,25 +46,25 @@ public class Latex {
 			}
 		}
 		out.append(end);
+		
 		String string = out.toString();
 		System.out.println(string);
+		
 		TeXIcon icon = new TeXFormula(string)
 					.createTeXIcon(TeXConstants.STYLE_DISPLAY,20);
 		icon.setInsets(new Insets(5,5,5,5));
+		
+		//TODO figure out how to wrap the text
 		return icon;
 	}
 
+	/**
+	 * Escapes special characters in latex so that they are displayed properly
+	 * This defends the way we format against injection
+	 * @param string of raw latex code
+	 * @return a string where the latex special characters are escaped
+	 */
 	public static String escape(String string) {
-//        "#"=>"\\#",
-//        "$"=>"\\$",
-//        "%"=>"\\%",
-//        "&"=>"\\&",
-//        "~"=>"\\~{}",
-//        "_"=>"\\_",
-//        "^"=>"\\^{}",
-//        "\\"=>"\\textbackslash",
-//        "{"=>"\\{",
-//        "}"=>"\\}",
 		string = string.replaceAll("\\\\(?!n)", "\\\\backslash");
 		string = string.replaceAll("~", "\\\\~");
 		string = string.replaceAll("#", "\\\\#");
@@ -69,7 +83,6 @@ public class Latex {
 			out.append(lines[i]);
 			out.append("} \\\\ ");
 		}
-		//TODO add support for newline. probably will need to split string on \n
 		return out.toString();
 	}
 	
