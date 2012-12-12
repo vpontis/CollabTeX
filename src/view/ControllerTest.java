afa30ca3869cf53a243a0f45bc2e16a349ccd97c
Deepak <Deepak@18.189.60.215>
2012-12-12 00:34:28 -0500

    Made changes to Controller Testing strategy.

--- src/view/ControllerTest.java
+++ src/view/ControllerTest.java
@@ -9,40 +9,44 @@ import server.Regex;
 
 
 /*
- * /**USABILITY TESTING on GUI - Once we confirmed that the logic was correct when went on to test the GUI.
+ * USABILITY TESTING on GUI -->
  * We began doing this by first testing a single client and then moving onto multiple clients. 
  * 
- * For single client testing we tried logging in with 25 random user names with random capitialzation  that all worked. We then confirmed that it was unable to login
- * when the field was left blank. We then moved onto testing the document table
- * We created multiple documents with multiple different names. They all worked as expected and were added to the table.
- * We check that the correct error was thrown when a document name was attempted to use again. We also tried clicking all the names and they all worked.
- * Next we decided to check the document. We enter a document and typed the enter ASCII alphabet at slow speed, medium speed and fast speed. We also tested
- * deleting in slow, medium, and fast speed. We also exited back out and made sure the name had been listed in the collab column.
- * In our editor any person who has made a change to the document is added to the collab column. We also checked to make sure our
- * LaTEx editor on various sample LaTex code. We also exited and entered the document 50 times very quickly to make sure it worked as expected which it did.Lasty
- * we created a document and logged off then logged back on to make sure we could reacess the document which we could
- * After all these tests we were satisfied that our GUI worked for single clients but still had to work for multiple users. We performed all these tasks on both
- * a MacBook Air running OSX 10.7 and a ThinkPad running Windows 7 to make sure we had satisfied the specs.
+ * For single client testing we tried logging in with 25 random user names with random capitalization that all worked. We 
+ * then confirmed that it was unable to login when the field was left blank. We then moved onto testing the document table
+ * We created multiple documents with multiple different names. 
+ * We check that the correct error was thrown when a duplicate document name was created. We also tried clicking all 
+ * the names and they all worked. Next we decided to check the document. We enter a document and typed the enter ASCII 
+ * alphabet at slow speed, medium speed and fast speed. We also tested deleting in slow, medium, and fast speed. We also 
+ * exited back out and made sure the name had been listed in the collab column. In our editor any person who has made a 
+ * change to the document is added to the collab column. We also checked to make sure our LaTEx editor on various sample 
+ * LaTex code. We also exited and entered the document 50 times very quickly to make sure it worked as expected which it did.
+ * Lastly we created a document and logged off then logged back on to make sure we could reacess the document which we could
+ * After all these tests we were satisfied that our GUI worked for single clients but still had to work for multiple users. 
+ * We performed all these tasks on both a MacBook Air running OSX 10.7 and a ThinkPad running Windows 7 to make sure we had 
+ * satisfied the specs.
  * 
  * For multiple client testing we had to make sure that everything worked as expected with multiple clients on the server.
- * We began by testing the login screen. We made sure that multiple users could login at similar times. We tried having 3 users to login in
- * within a small time period and it worked well. We also tried to enter the same user name and the correct error message was shown. Once this
- * was completed we moved on to testing the table. We had each user create a document and make sure that the document appeared on the 
- * other two clients table. We also checked at this point that the last modified time was as expected. We had each of the 3 users created 10 documents
- * and everything worked as expected. We then moved onto testing the document itself which was the hardest to test. We first had all three users enter the same 
- * document. We began by making sure each user was added to the collab list at the top. Then we begin typing and deleting slowly on different lines to confirm the easiest functionality. This worked as expected with each client seeing the 
- * work of the other two people without a noticeable time lag. We then increased the typing and deleting speed to medium and then fast and got the results we expected
- * Now we had to worry about the tougher testing part with concurrency. First we had all users type on the same line but just alternating letters. We would tell user 1 
- * to type an a then user 2 to type a b then user 3 to type a c. We saw what we expected. Then we had each user move their cursor away but on the same line and start jaming on the keyboard
- * again the results were expected. Next we had one user start typing and had the other two users typing on the same spot on the document. We did this with insertion as well as deletion. 
- * We repeated these tests at multiple speeds and with both OSX and Window and did not encounter very few errors. Sometimes at very high speed the cursor would skip back a spot but that only 
- * Occurred at a low percentage in a unlikely test case, so we labeled it an acceptable error. After repeating these test 10's of time we were convinced it work. We then went back to the document
- * table and made sure each user was added to the collab column in the table. We repeated this for a few different documents and was convinced our document had no major concurrency issues. We then disconnected
- * each document from the server and then reconnected to make sure the changes were saved and that the server could recover the document.
+ * We began by testing the login screen. We made sure that multiple users could login at similar times. We tried having 3 users to 
+ * login in within a small time period and it worked well. We also tried to enter the same user name and the correct error message 
+ * was shown. Once this was completed we moved on to testing the table. We had each user create a document and make sure that the 
+ * document appeared on the other two clients table. We also checked at this point that the last modified time was as expected. 
+ * We had each of the 3 users created 10 documents and everything worked as expected. We then moved onto testing the document itself 
+ * which was the hardest to test. We first had all three users enter the same document. We began by making sure each user was added 
+ * to the collab list at the top. Then we begin typing and deleting slowly on different lines to confirm the easiest functionality. 
+ * This worked as expected with each client seeing the work of the other two people without a noticeable time lag. We then increased 
+ * the typing and deleting speed to medium and then fast and got the results we expected. Now we had to worry about the tougher testing 
+ * part with concurrency. First we had all users type on the same line but just alternating letters. We would tell user 1 
+ * to type an a then user 2 to type a b then user 3 to type a c. We saw what we expected. Then we had each user move their cursor away 
+ * but on the same line and start jamming on the keyboard again the results were expected. Next we had one user start typing and had the 
+ * other two users typing on the same spot on the document. We did this with insertion as well as deletion. We repeated these tests at 
+ * multiple speeds and with both OSX and Window and did not encounter very few errors. Sometimes at very high speed the cursor would skip 
+ * back a spot but that only occurred at a low percentage in a unlikely test case, so we labeled it an acceptable error. After repeating 
+ * these test 10's of time we were convinced it work. We then went back to the document table and made sure each user was added to the 
+ * collab column in the table. We repeated this for a few different documents and was convinced our document had no major concurrency issues. 
+ * We then disconnected each document from the server and then reconnected to make sure the changes were saved and that the server could 
+ * recover the document.
  * 
- * Overall the testing process took about 60 minutes and was repeated anytime we made a major push to the repo (This was usually about once a day). We 
- * wanted to make sure we caught errors early so we could traceback what push or commit they were introduced by. Overall our manual GUI testing was very time 
- * consuming but a crucial part in making sure our project was working as expected.
  *  
  */
 
